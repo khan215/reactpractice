e60540d3967c56d32e30a1d63aa7124e37cbd52a
Dawood Khan <dawoodkhan215@gmail.com>
2020-01-27 01:44:03 +0500

    commit

---
+++ components/dashboard/Notifications.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notifications = () => {
+    return (
+        <div>
+            Notifications Here
+        </div>
+    )
+}
+
+export default Notifications
---
+++ components/projects/ProjectList.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+ const ProjectList = () => {
+    return (
+        <div className='project-list section'>
+            
+            <div className='card z-depth-0 project-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Project Title</span>
+                    <p>Posted by the Dawood Shamim</p>
+                    <p className='grey-text'>15th February 1995</p>
+                </div>
+            </div>
+
+            <div className='card z-depth-0 project-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Project Title</span>
+                    <p>Posted by the Dawood Shamim</p>
+                    <p className='grey-text'>15th February 1995</p>
+                </div>
+            </div>
+
+            <div className='card z-depth-0 project-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Project Title</span>
+                    <p>Posted by the Dawood Shamim</p>
+                    <p className='grey-text'>15th February 1995</p>
+                </div>
+            </div>
+
+            <div className='card z-depth-0 project-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Project Title</span>
+                    <p>Posted by the Dawood Shamim</p>
+                    <p className='grey-text'>15th February 1995</p>
+                </div>
+            </div>
+
+            <div className='card z-depth-0 project-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Project Title</span>
+                    <p>Posted by the Dawood Shamim</p>
+                    <p className='grey-text'>15th February 1995</p>
+                </div>
+            </div>
+
+            <div className='card z-depth-0 project-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Project Title</span>
+                    <p>Posted by the Dawood Shamim</p>
+                    <p className='grey-text'>15th February 1995</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ProjectList
