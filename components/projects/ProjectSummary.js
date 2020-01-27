653a2f36c09fae3048761d8272339a86d2560223
dawood.shamim <dawood.shamim>
2020-01-27 01:43:32 -0800

    project summary

---
+++ components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className='card z-depth-0 project-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Project Title</span>
+                    <p>Posted by the Dawood Shamim</p>
+                    <p className='grey-text'>15th February 1995</p>
+                </div>
+            </div>
+    )
+}
+
+
+export default ProjectSummary
